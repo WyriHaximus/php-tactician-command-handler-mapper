990730b41226b7e466dbe5ac870e576b9b243f5e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-05 18:49:15 +0200

    Basic true test

---
+++ tests/BasicTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WyriHaximus\Tests\Tactician\CommandHandler;
+
+class BasicTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
